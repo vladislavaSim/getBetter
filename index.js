b3295eed7d2ad265f97d58c913551d0beff37589
vladislavaSim <simonova.vladislava@gmail.com>
2021-12-17 14:41:58 +0200

    english added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-let taskList = [
+let taskListRu = [
     'Сходить на выставку',
-'Убрать мусор в лесу, в парке или у реки. Можно позвать друзей на помощь!',
+    'Убрать мусор в лесу, в парке или у реки. Можно позвать друзей на помощь!',
     'Извиниться перед человеком, которого обидел',
     'Покормить бездомное животное',
     'Сделать небольшой подарок тому, кто этого не ожидает',
@@ -19,20 +19,73 @@ let taskList = [
     'Выучить новое слово',
     'Пойти на прогулку',
     'Пожертвовать на благотворительность',
-    // 'Помочь каждому, кто попросит о помощи сегодня',
-    // 'День без соцсетей',
-    // 'День без переедания',
-    // 'Написать приятные комментарии под фото трём людям',
+    'Помочь каждому, кто попросит о помощи сегодня',
+    'День без соцсетей',
+    'День без переедания',
+    'Написать приятные комментарии под фото трём людям',
+    'Сделай кормушку для птиц'
+];
+
+let taskListEn = [
+    'Go to the exhibition',
+    'Pick up all the rubbish in a forest, park or riverside. Call your friends for help!',
+    'Apologize to the person you offended',
+    'Feed a stray animal',
+    'Make a small gift to someone who doesn\'t expect it',
+    'No bad words day',
+    'Make a dish you\'ve never cooked before',
+    'Clean up the room',
+    'Compliment a stranger',
+    'Meet a friend',
+    'Do 10 squats',
+    'No Complaints Day',
+    'Do what you put off for a long time',
+    'Eat fruit or vegetable salad',
+    'Go to bed earlier',
+    'Thank people out loud throughout the day',
+    'Write someone who you haven\'t talked for a long time',
+    'Learn a new word',
+    'Go for a walk',
+    'Donate to charity',
+    'Help everyone who asks for help today',
+    'No social networks day',
+    'No overeating day',
+    'Write nice comments under the photo to three people',
+    'Make a feeder for birds'
 ];
 let $container = document.querySelector('.container')
 let $button = document.querySelector('.button')
 let $textBox = document.createElement('div');
 $textBox.classList.add('text')
 
-$button.addEventListener('click', function () {
-    let index =  Math.floor(Math.random() * taskList.length);
-    $button.innerHTML = 'Далее'
-    $textBox.innerText = taskList[index]
-    $container.appendChild($textBox)
+let array = [taskListRu, taskListEn];
+$button.innerHTML = 'стать лучше';
+let $langSwitchBtn = document.querySelector('.language');
+$langSwitchBtn.innerHTML = 'ENG'
+switchLanguage(array[0])
+function switchLanguage(arr) {
+        $button.addEventListener('click', function () {
+            let index =  Math.floor(Math.random() * arr.length);
+            $textBox.innerText = arr[index]
+            $container.appendChild($textBox)
+        })
+}
+
+let activeLangRus = true;
+$langSwitchBtn.addEventListener('click', function (){
+    if(activeLangRus) {
+        $button.innerHTML = 'get better'
+        $langSwitchBtn.innerHTML = 'RUS'
+    switchLanguage(array[1])
+        return activeLangRus = false
+    }
+    if(!activeLangRus){
+        $button.innerHTML = 'стать лучше';
+        $langSwitchBtn.innerHTML = 'ENG'
+    switchLanguage(array[0])
+        return activeLangRus = true
+    }
 })
+
+
 
